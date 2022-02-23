de9a347a64fe7c5ff0d3d10c180b6b551544613e
Apton-riyazS <riyaz.s@aptonworks.com>
2022-02-23 22:02:05 +0530

    last commit in google python exercise

--- google_python_exercise/python_babyname.py
+++ google_python_exercise/python_babyname.py
@@ -2,7 +2,8 @@
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
-
+#python3 /home/riyaz/git/summary.txt file /home/riyaz/Downloads/google-python-exercises/babynames/baby1990.html
+#./baby3.py /home/riyaz/git/summary.txt file /home/riyaz/Downloads/google-python-exercises/babynames/baby1990.html
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
